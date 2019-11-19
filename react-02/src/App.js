2e417cfc0e4fb41da8d9a9ad98f66dba2f06a000
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2019-11-19 16:05:03 -0700

    upd 02-react app files

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,35 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 import Icons from "./components/MyIcons.js"
+import Homepage from "./components/MyHomepage.js"
+import Game from "./components/MyGame.js"
+
+import explosionIcon from 'C:/code/cohort3/react-02/src/images/explosion-icon.svg';
+import houseIcon from 'C:/code/cohort3/react-02/src/images/house-icon.svg';
+import nuclearAtomIcon from 'C:/code/cohort3/react-02/src/images/nuclear-atom-icon.svg';
+import nuclearRadiationIcon from 'C:/code/cohort3/react-02/src/images/nuclear-radiation-icon.svg';
+import ticTacToeIcon from 'C:/code/cohort3/react-02/src/images/tic-tac-toe-icon.svg';
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      selected: nuclearAtomIcon
+    }
+  }
+
   selectedElement = (event) => {
     this.setState({
-        selected: event.target.name
+      selected: event.target.name
     });
     console.log(event.target.name);
-}
+  }
+
+  navIconMapper = () => {
+    const images = [nuclearAtomIcon, ticTacToeIcon, explosionIcon, nuclearRadiationIcon, houseIcon];
+    return images.map((image, i) => <img key={i} name={image} src={image} tabIndex={0} className={`icon ${image}`} alt={`icon ${image}`} onClick={this.selectedElement} />);
+  }
+
+  displayScreen = () => {
+    if (this.state.selected === nuclearAtomIcon) {
+      return < Homepage />;
+    } if (this.state.selected === ticTacToeIcon) {
+      return < Game />;
+    } if (this.state.selected === explosionIcon || this.state.selected === nuclearRadiationIcon || this.state.selected === houseIcon) {
+      return < Homepage />;
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Icons selectedElement={this.selectedElement}/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
+          <Icons navIconMapper={this.navIconMapper} />
         </header>
+        <div className="App-display">
+          {this.displayScreen()}
+        </div>
       </div>
     );
   }
--- react-02/src/components/MyIcons.js
+++ react-02/src/components/MyIcons.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import explosionIcon from './images/explosion-icon.svg';
-import houseIcon from './images/house-icon.svg';
-import nuclearAtomIcon from './images/nuclear-atom-icon.svg';
-import nuclearRadiationIcon from './images/nuclear-radiation-icon.svg';
-import reactIcon from './images/react-icon.svg';
-
 
 class Icons extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            selected: ""
-        }
-    }
-
-    imageMapper = () => {
-        const images = [nuclearAtomIcon, explosionIcon, reactIcon, houseIcon, nuclearRadiationIcon];
-        return images.map((image, i) => <img key={i} name={image} src={image} tabIndex={i} className={`icon ${image}`} alt={`icon ${image}`} onClick={this.props.selectedElement} />);
-    }
-
     render() {
         return (
-            <div className="nav">
-                {this.imageMapper()}
+            <div className="navbar">
+                {this.props.navIconMapper()}
             </div>
-        )
+        );
     }
 }
 
