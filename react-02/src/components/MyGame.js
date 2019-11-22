ce153536af1bc3ee65c490705ad9c39469606d04
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2019-11-22 11:50:48 -0700

    upd MyGame.js

--- react-02/src/components/MyGame.js
+++ react-02/src/components/MyGame.js
@@ -28,7 +28,6 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    console.log(squares + " initial square state");
 
     if (this.calculateWinner(squares) || squares[i]) {
       return;
@@ -42,7 +41,6 @@ class Game extends React.Component {
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
-    console.log(squares + " end square state");
   }
 
   jumpTo(step) {
