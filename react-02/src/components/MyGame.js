c4ebe2ff7d91759ae8f10bdb0cf5873317de6c0b
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2019-11-25 15:44:55 -0700

    upd tic tac toe unbeatable ai

--- react-02/src/components/MyGame.js
+++ react-02/src/components/MyGame.js
@@ -12,16 +12,17 @@ class Game extends React.Component {
       xIsNext: true,
       startGame: false,
       computerTurn: false,
-      computerIsX: false,
+      humanPlayer: "X",
+      computerPlayer: "O",
     }
   }
 
   firstPlayer(event) {
     const booleanState = (event.target.value === "true");
     if (this.state.startGame === false) {
-      this.setState(() => ({
+      this.setState({
         computerTurn: booleanState,
-      }));
+      });
     }
   }
 
@@ -32,24 +33,111 @@ class Game extends React.Component {
     });
     if (this.state.computerTurn === true && this.state.startGame === true) {
       await this.setState({
-        computerIsX: booleanState,
+        computerPlayer: "X",
+        humanPlayer: "O",
       });
       return this.compDecision(this.state.history[0].squares);
     } return;
   }
 
-  // minimax?
+  // --- Computer AI (unbeatable) ---
+  // Logic (miniMax) inspired by ahmad abdolsaheb at https://www.freecodecamp.org/news/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37/
+
   compDecision(board) {
     setTimeout(() => {
       if (this.state.computerTurn === true && this.state.startGame === true) {
-        const is_null = (index) => {
-          return index === null;
-        }
-        const pick = board.findIndex(is_null);
-        return this.handleClick(pick);
+        const pick = this.miniMax(board, this.state.computerPlayer);
+        this.handleClick(pick.index);
       }
-    }, 1500);
+    }, 1000);
   }
+
+  miniMax(board, player) {
+    let open_spots = this.emptyIndexes(board);
+    let board_copy = board.slice(0);
+
+    if (this.winningMoves(board_copy, this.state.humanPlayer)) {
+      return { score: -10 };
+    } else if (this.winningMoves(board_copy, this.state.computerPlayer)) {
+      return { score: 10 };
+    } else if (open_spots.length === 0) {
+      return { score: 0 };
+    }
+
+    let moves = [];
+
+    for (let i = 0; i < open_spots.length; i++) {
+      let move = {};
+      let corresponding_index = (index) => {
+        return index === board_copy[open_spots[i]];
+      }
+      move.index = board_copy.findIndex(corresponding_index);
+
+      board_copy[open_spots[i]] = player;
+
+      if (player === this.state.computerPlayer) {
+        let result1 = this.miniMax(board_copy, this.state.humanPlayer);
+        move.score = result1.score;
+      }
+      else {
+        let result2 = this.miniMax(board_copy, this.state.computerPlayer);
+        move.score = result2.score;
+      }
+
+      board_copy[open_spots[i]] = move.index;
+      moves.push(move);
+    }
+
+    let best_pick;
+    if (player === this.state.computerPlayer) {
+      let best_score = -10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score > best_score) {
+          best_score = moves[i].score;
+          best_pick = i;
+        }
+      }
+    }
+    else {
+      let best_score = 10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score < best_score) {
+          best_score = moves[i].score;
+          best_pick = i;
+        }
+      }
+    }
+    return moves[best_pick];
+  }
+
+  emptyIndexes(board) {
+    var open_spots = [];
+    for (var i = 0; i < board.length; i++) {
+      if (board[i] !== "O" && board[i] !== "X") {
+        open_spots.push(i);
+      }
+    }
+    return open_spots;
+  }
+
+  winningMoves(board, player) {
+    if (
+      (board[0] === player && board[1] === player && board[2] === player) ||
+      (board[3] === player && board[4] === player && board[5] === player) ||
+      (board[6] === player && board[7] === player && board[8] === player) ||
+      (board[0] === player && board[3] === player && board[6] === player) ||
+      (board[1] === player && board[4] === player && board[7] === player) ||
+      (board[2] === player && board[5] === player && board[8] === player) ||
+      (board[0] === player && board[4] === player && board[8] === player) ||
+      (board[2] === player && board[4] === player && board[6] === player)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // --- ---
 
   async handleClick(i) {
     const location = [
@@ -90,12 +178,9 @@ class Game extends React.Component {
       stepNumber: step,
       xIsNext: (step % 2) === 0,
     });
-    if (this.state.xIsNext === true && this.state.computerIsX === true) {
-      await this.setState({
-        computerTurn: true,
-      });
-      return this.compDecision(this.state.history[step].squares);
-    } else if (this.state.xIsNext === false && this.state.computerIsX === false) {
+    if (
+      (this.state.xIsNext === true && this.state.computerPlayer === "X") ||
+      (this.state.xIsNext === false && this.state.computerPlayer === "O")) {
       await this.setState({
         computerTurn: true,
       });
@@ -149,7 +234,7 @@ class Game extends React.Component {
             <Board
               winningRow={winner ? winner.row : []}
               squares={current.squares}
-              onClick={(i) => this.handleClick(i)}
+              onClick={(i) => { if (this.state.computerTurn === false && this.state.startGame === true) { this.handleClick(i) } }} //refactor conditional out?
             />
           </div>
           <div className="game-info">
