a25e348e48b3632c3a4374181d900933857ccebe
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2019-11-30 16:08:29 -0700

    upd accounts, css

--- react-02/src/App.test.js
+++ react-02/src/App.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Game from "./components/tic-tac-toe/MyGame.js";
 import Accounts from "./components/accounts/MyAccounts.js";
 
-it('renders without crashing', () => {
+it('App renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Tic-Tac-Toe renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Game />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Accounts without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Accounts />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- react-02/src/components/accounts/MyAccountsCards.js
+++ react-02/src/components/accounts/MyAccountsCards.js
@@ -5,27 +5,29 @@ class AccountCardsList extends React.Component {
     render() {
         const accountCards = this.props.listArray.map((account, i) => {
             return (
-                <form
+                <div 
                     key={account.key}
                     id={account.key}
                     className="account-card">
-                    <label id={account.key} className="account-name">
-                        {account.accountName}
-                    </label>
-                    <label id={account.key} className="account-balance">
-                        {`$${(account.startingBalance).toFixed(2)}`}
-                    </label>
-                    <input type="number" id={account.key} className="account-input" name="activeBalance" onChange={this.props.handleOnChange} ref={this.props.clearInput}/>
-                    <input type="button" id={account.key} value="Deposit" className="deposit-button account-button" onClick={() => this.props.handleDeposit(i)} />
-                    <input type="button" id={account.key} value="Withdraw" className="withdraw-button account-button" onClick={() => this.props.handleWithdraw(i)} />
-                    <input type="button" id={account.key} value="Delete Account" className="delete-button account-button" onClick={() => this.props.handleDelete(i)}/>
-                </form>
+                    <form id={account.key}>
+                        <label id={account.key} className="account-name">
+                            {account.accountName}
+                        </label>
+                        <label id={account.key} className="account-balance">
+                            {`$${(account.startingBalance).toFixed(2)}`}
+                        </label>
+                        <input type="number" id={account.key} min="0.01" step="0.01" className="account-input" name="changeBalance" onChange={this.props.handleOnChange} ref={this.props.clearInput} />
+                        <input type="button" id={account.key} value="Deposit" className="deposit-button account-button" onClick={() => this.props.handleDeposit(i)} />
+                        <input type="button" id={account.key} value="Withdraw" className="withdraw-button account-button" onClick={() => this.props.handleWithdraw(i)} />
+                        <input type="button" id={account.key} value="Delete Account" className="delete-button account-button" onClick={() => this.props.handleDelete(i)} />
+                    </form>
+                </div>
             )
         }
         );
 
         return (
-            <div>
+            <div className="account-display">
                 {accountCards}
             </div>
         )
