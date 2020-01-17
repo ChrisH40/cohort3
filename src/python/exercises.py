746bdc04f6e2844679763e256bc1f4cec743c8d1
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2020-01-16 21:34:09 -0700

    upd python exercises

--- src/python/exercises.py
+++ src/python/exercises.py
@@ -2,7 +2,32 @@
 
 # --- Coding Exerise 7: @classmethod and @staticmethod ---
 
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
 
+    def add_item(self, name, price):
+        self.items.append({
+            'name': name,
+            'price': price
+        })
+
+    def stock_price(self):
+        total = 0
+        for item in self.items:
+            total += item['price']
+        return total
+
+    @classmethod
+    def franchise(cls, store):
+        new_store = cls(store.name + " - franchise")
+        return new_store
+
+    @staticmethod
+    def store_details(store):
+        return "{}, total stock price: {}".format(store.name, int(store.stock_price()))
+        
 
 # --- Coding Exerise 6: Classes and objects ---
 # 1. The __init__ method, which should take an argument, name. It should initialize self.name to be the argument, and self.items to be an empty list.
