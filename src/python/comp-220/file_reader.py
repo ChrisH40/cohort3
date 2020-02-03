ff07ba2cce5f10348bd67ead0b4cce1d673dc073
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2020-02-02 20:18:47 -0700

    upd file_reader, test_file_reader

--- src/python/comp-220/file_reader.py
+++ src/python/comp-220/file_reader.py
@@ -2,7 +2,7 @@
 class FileReader:
 
     def __init__(self, file):
-        self.xfile_name = file
+        self.name = file
         self.xfile = open(file, 'r').readlines()
 
     def line_count(self):
@@ -23,8 +23,7 @@ class FileReader:
         return chars
 
     def file_summary(self, word):
-        name = self.xfile_name
         lines = self.line_count()
         chars = self.char_count()
         words = self.word_count(word)
-        return f"File {name} has: \n- {lines} lines \n- {chars} characters \n- {words} instances of the word '{word}'"
+        return f"File {self.name} has: \n- {lines} lines \n- {chars} characters (incl. spaces) \n- {words} instances of the word '{word}'"
--- src/python/comp-220/test_file_reader.py
+++ src/python/comp-220/test_file_reader.py
@@ -9,12 +9,12 @@ def test_FileReader():
         "/code/cohort3/src/python/comp-220/dummy-syntax.js")
     assert reader.line_count() == 14
     assert reader.word_count('else') == 3
-    assert reader.char_count() == 299
+    assert reader.char_count() == 307
     assert reader.file_summary(
-        "else") == "File /code/cohort3/src/python/comp-220/dummy-syntax.js has: \n- 14 lines \n- 299 characters \n- 3 instances of the word 'else'"
+        "else") == "File /code/cohort3/src/python/comp-220/dummy-syntax.js has: \n- 14 lines \n- 307 characters (incl. spaces) \n- 3 instances of the word 'else'"
     reader = file_reader.FileReader("/code/cohort3/src/javascript/syntax.js")
     assert reader.line_count() == 247
     assert reader.word_count('else') == 10
     assert reader.char_count() == 7626
     assert reader.file_summary(
-        "else") == "File /code/cohort3/src/javascript/syntax.js has: \n- 247 lines \n- 7626 characters \n- 10 instances of the word 'else'"
+        "else") == "File /code/cohort3/src/javascript/syntax.js has: \n- 247 lines \n- 7626 characters (incl. spaces) \n- 10 instances of the word 'else'"
