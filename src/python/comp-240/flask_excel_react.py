566bb8d09eb3f8a230ced02c32c4a3a881bdaa30
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2020-02-19 11:37:32 -0700

    upd flask_excel_react.py, index.html - add data display loop

--- src/python/comp-240/flask_excel_react.py
+++ src/python/comp-240/flask_excel_react.py
@@ -1,19 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import sys
 sys.path.append('/code/cohort3/src/python/comp-230/')
 from invoice_sheet_functions import sheet_reader
 
 app = Flask(__name__)
 
-sheet_data = sheet_reader('/code/cohort3/src/python/comp-230/invoice_data.xlsx')
+sheet_data = sheet_reader(
+    '/code/cohort3/src/python/comp-230/invoice_data.xlsx')
 
 
 @app.route('/')
 def home():
+    try:
+        return jsonify({'message': 'Use url extension raw or loop to display data.'})
+    except:
+        return jsonify({'message': 'Error. Please try again.'})
+
+
+@app.route('/raw')
+def data_dump():
     try:
         return jsonify(sheet_data)
     except:
         return jsonify({'message': 'Error. Please try again.'})
 
 
-app.run(port=5000)
+@app.route('/loop')
+def data_loop():
+    try:
+        return render_template('index.html', data=sheet_data)
+    except:
+        return jsonify({'message': 'Error. Please try again.'})
+
+
+app.run(port=5000, debug=True)
