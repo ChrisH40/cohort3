42973d2aa1c303d2767d04eec2ec5499c2d75ecc
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2020-02-20 10:34:08 -0700

    upd city_data_reader.py - split func into 2, upd test file

--- src/python/comp-220/city_data_reader.py
+++ src/python/comp-220/city_data_reader.py
@@ -2,7 +2,6 @@ import csv
 
 
 def city_data_reader(file):
-
     with open(file, "r") as csvfile:
         datareader = csv.reader(csvfile)
         header = next(datareader)
@@ -11,13 +10,18 @@ def city_data_reader(file):
         RES_CNT = header.index('RES_CNT')
         class_totals = dict()
         sect_totals = dict()
+        data = []
         for row in datareader:
-            class_totals[row[CLASS]] = class_totals.get(
-                row[CLASS], 0) + int(row[RES_CNT])
-            sect_totals[row[SECTOR]] = sect_totals.get(
-                row[SECTOR], 0) + int(row[RES_CNT])
+            class_totals[row[CLASS]] = class_totals.get(row[CLASS], 0) + int(row[RES_CNT])
+            sect_totals[row[SECTOR]] = sect_totals.get(row[SECTOR], 0) + int(row[RES_CNT])
+        data.append(class_totals)
+        data.append(sect_totals)
     csvfile.close()
+    city_report_generator(data, file)
+    return data
 
+
+def city_report_generator(data, file):
     with open('report.txt', 'w') as report:
         total_report_rows = 0
         report.write("*** CITY DATA READER ***\n")
@@ -25,12 +29,12 @@ def city_data_reader(file):
         report.write("\n====================\n")
         report.write("\nReport:\n")
         report.write('{:30} {}'.format("\nCLASS", "RES_CNT Total\n\n"))
-        for key, value in class_totals.items():
+        for key, value in data[0].items():
             report.write('{:30} {}'.format(f"{key}", f"{value}\n"))
             total_report_rows += 1
         report.write("\n-----\n")
         report.write('{:30} {}'.format("\nSECTOR", "RES_CNT Total\n\n"))
-        for key, value in sect_totals.items():
+        for key, value in data[1].items():
             report.write('{:30} {}'.format(f"{key}", f"{value}\n"))
             total_report_rows += 1
         report.write("\n-----\n")
@@ -41,5 +45,4 @@ def city_data_reader(file):
     return report
 
 
-city_data_reader(
-    "/code/cohort3/src/python/comp-220/Census_by_Community_2018.csv")
+city_data_reader("/code/cohort3/src/python/comp-220/Census_by_Community_2018.csv")
--- src/python/comp-220/test_city_data_reader.py
+++ src/python/comp-220/test_city_data_reader.py
@@ -2,8 +2,18 @@ import pytest
 import city_data_reader
 
 
-def test_CityDataReader():
-      report = city_data_reader.city_data_reader("/code/cohort3/src/python/comp-220/Census_by_Community_2018.csv")
+def test_city_data_reader():
+      data = city_data_reader.city_data_reader("/code/cohort3/src/python/comp-220/Census_by_Community_2018.csv")
+      assert data[0] == {'Residential': 1263734, 'Industrial': 922, 'Major Park': 0, 'Residual Sub Area': 0}
+      assert data[1] == {'CENTRE': 199977, 'SOUTH': 227345, 'NORTHEAST': 185534, 'NORTHWEST': 174126, 'NORTH': 160502, 'SOUTHEAST': 135009, 'EAST': 57666, 'WEST': 124497}
+
+
+def test_city_report_generator():
+      data = [
+            {'Residential': 1263734, 'Industrial': 922, 'Major Park': 0, 'Residual Sub Area': 0},
+            {'CENTRE': 199977, 'SOUTH': 227345, 'NORTHEAST': 185534, 'NORTHWEST': 174126, 'NORTH': 160502, 'SOUTHEAST': 135009, 'EAST': 57666, 'WEST': 124497}
+      ]
+      report = city_data_reader.city_report_generator(data, "/code/cohort3/src/python/comp-220/Census_by_Community_2018.csv")
       with open(report.name, "r") as test_report:
             text = test_report.readlines()
             assert len(text) == 35
