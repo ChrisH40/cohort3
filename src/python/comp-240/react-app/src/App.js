94fdeeb8b8c3c8aac30d2ecd9630a0e681d5c7cf
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2020-03-08 18:50:50 -0600

    refactor data loop

--- src/python/comp-240/react-app/src/App.js
+++ src/python/comp-240/react-app/src/App.js
@@ -19,7 +19,6 @@ const App = () => {
     return () => { fetched = true; }
   }, [sheet]);
 
-
   const handleSubmit = (event) => {
     if (sheetSubmit === "Customers"
       || sheetSubmit === "Invoices"
@@ -35,6 +34,16 @@ const App = () => {
     event.preventDefault();
   };
 
+  const itemDecon = (item) => {
+    let items = Object.keys(item);
+    return items.map(key => {
+      let value = item[key];
+      return (
+        <td key={key}>{value}</td>
+      )
+    });
+  };
+
   const displayHeader = (data) => {
     if (data.length > 0) {
       let header = Object.keys(data[0])
@@ -45,72 +54,16 @@ const App = () => {
     else return null;
   };
 
-  // Re-factor to eliminate if/else statements (map with in a map?)...
   const displaySheet = (data) => {
     if (data.length > 0) {
-      if (sheet === 'Customers') {
-        return data.map((item, i) => {
-          return (
-            <tr key={i}>
-              <td>{item['CUST_ID']}</td>
-              <td>{item['F_NAME']}</td>
-              <td>{item['L_NAME']}</td>
-              <td>{item['APT_NUM']}</td>
-              <td>{item['ST_NUM']}</td>
-              <td>{item['ST_NAME']}</td>
-              <td>{item['CITY']}</td>
-              <td>{item['PROV']}</td>
-              <td>{item['CTRY']}</td>
-              <td>{item['P_CODE']}</td>
-              <td>{item['PHONE']}</td>
-              <td>{item['EMAIL']}</td>
-            </tr>
-          )
-        })
-      }
-      else if (sheet === 'Invoices') {
-        return data.map((item, i) => {
-          return (
-            <tr key={i}>
-              <td>{item['INV_ID']}</td>
-              <td>{item['CUST_ID']}</td>
-              <td>{item['INV_DATE']}</td>
-              <td>{item['INV_DESC']}</td>
-            </tr>
-          )
-        })
-      }
-      else if (sheet === 'Products') {
-        return data.map((item, i) => {
-          return (
-            <tr key={i}>
-              <td>{item['PROD_ID']}</td>
-              <td>{item['PROD_NAME']}</td>
-              <td>{item['PROD_DESC']}</td>
-              <td>{item['SKU']}</td>
-              <td>{item['UNIT_PRICE']}</td>
-            </tr>
-          )
-        })
-      }
-      else if (sheet === 'Line Items') {
-        return data.map((item, i) => {
-          return (
-            <tr key={i}>
-              <td>{item['LINE_ID']}</td>
-              <td>{item['INV_ID']}</td>
-              <td>{item['PROD_ID']}</td>
-              <td>{item['QTY']}</td>
-            </tr>
-          )
-        })
-      }
-    }
-    else return (
-      <tr key={1}>
-        <td>Please Activate Server/Input Data...</td>
-      </tr>
-    )
+      return data.map((item, i) => {
+        return (
+          <tr key={i}>
+            {itemDecon(item)}
+          </tr>
+        )
+      })
+    };
   };
 
   return (
